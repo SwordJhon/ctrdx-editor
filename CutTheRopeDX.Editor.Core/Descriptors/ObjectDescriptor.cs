5d672708122cc17afcb791c8f6d2f88624cf71b0
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2026-07-03 13:07:53 +0700

    refactor: Update project namespace, centralize package manager (#2)
    * add explicit using
    
    * rename project
    
    * Create Directory.Build.props
    
    * use central package manager
    
    * Create nuget.config
    
    * centralize project options
    
    * upgrade packages

--- CutTheRopeDX.Editor.Core/Descriptors/ObjectDescriptor.cs
+++
@@ -1,9 +0,0 @@
-namespace CutTheRopeDX.Editor.Core.Descriptors
-{
-    /// <summary><paramref name="MaxCount"/> of <see cref="int.MaxValue"/> means unbounded.</summary>
-    public sealed record ObjectDescriptor(
-        string ElementName,
-        string DisplayName,
-        IReadOnlyList<AttributeSpec> Attributes,
-        int MaxCount);
-}
